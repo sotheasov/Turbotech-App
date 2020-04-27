2f18adb45f2a73f9c14b755d1f4f0d9d73985901
Em Vannet <emvannet@Ems-MacBook-Pro.local>
2020-04-27 15:57:09 +0700

    Create Carousel

--- turbotechapp/ViewControllers/HomeViewController.swift
+++ turbotechapp/ViewControllers/HomeViewController.swift
@@ -8,23 +8,62 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate {
+   
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageView: UIPageControl!
+    var arrayImage = [
+        UIImage(named: "picslider1"),
+        UIImage(named: "picslider3"),
+        UIImage(named: "picslider3"),
+        UIImage(named: "picslider1"),
+        UIImage(named: "picslider3"),
+        UIImage(named: "picslider3"),
+    ]
+    var time = Timer()
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        sliderCollectionView.dataSource = self
+        sliderCollectionView.delegate = self
+        
+        pageView.numberOfPages = arrayImage.count
+        pageView.currentPage = 0
+        
+        DispatchQueue.main.async {
+            self.time = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func changeImage(){
+        if counter < arrayImage.count {
+               let index = IndexPath.init(item: counter, section: 0)
+               self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+               pageView.currentPage = counter
+               counter += 1
+           } else {
+               counter = 0
+               let index = IndexPath.init(item: counter, section: 0)
+               self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
+               pageView.currentPage = counter
+               counter = 1
+           }
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        arrayImage.count
+    }
+       
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath);
+        if let vc = cell.viewWithTag(111) as? UIImageView{
+            vc.image = arrayImage[indexPath.row]
+        }
+        else if let ab = cell.viewWithTag(222) as? UIPageControl{
+            ab.currentPage = indexPath.row
+        }
+        return cell
+    }
 }
